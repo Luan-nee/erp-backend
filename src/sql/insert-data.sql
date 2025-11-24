7eb922fad8f6f0b157fa365d1dce0c5bc75dd349
Luan-nee <luandelsol54@gmail.com>
2025-11-24 16:09:18 -0500

    agregando comillas invertidas a los nombres de las tablas en inserciones

--- src/sql/insert-data.sql
+++ src/sql/insert-data.sql
@@ -1,7 +1,7 @@
 USE `erp_app`;
 
 -- tipos_tax
-INSERT INTO tipos_tax (nombre, codigo_sunat, porcentaje_tax) VALUES
+INSERT INTO `tipos_tax` (nombre, codigo_sunat, porcentaje_tax) VALUES
 ('Gravado - Operación Onerosa', '10', 0.18),
 ('Exonerado - Operación Onerosa', '20', 0),
 ('Exonerado - Transferencia gratuita', '21', 0),
@@ -9,7 +9,7 @@ INSERT INTO tipos_tax (nombre, codigo_sunat, porcentaje_tax) VALUES
 ('Exportación de Bienes o Servicios', '40', 0);
 
 -- estados_doc_facturacion
-INSERT INTO estados_doc_facturacion (codigo, nombre) VALUES
+INSERT INTO `estados_doc_facturacion` (codigo, nombre) VALUES
 ('01', 'Registrado en servidor factpro (Se puede editar el comprobante)'),
 ('03', 'Enviado pero sin respuesta de la sunat'),
 ('05', 'Aceptado ante la sunat'),
@@ -29,21 +29,21 @@ INSERT INTO `metodos_pago` (`nombre`, `estaActivado`) VALUES
 ('Plin', FALSE);
 
 -- colores
-INSERT INTO colores (nombre, hex) VALUES
+INSERT INTO `colores` (nombre, hex) VALUES
 ('Rojo', '#FF0000'),
 ('Azul', '#0000FF'),
 ('Verde', '#008000'),
 ('Amarillo', '#FFFF00');
 
 -- categorias 
-INSERT INTO categorias (nombre, descripcion) VALUES
+INSERT INTO `categorias` (nombre, descripcion) VALUES
 ('Electrónica', 'Dispositivos y accesorios tecnológicos como laptops, móviles y audífonos.'),
 ('Ropa y Moda', 'Artículos de vestimenta, calzado y accesorios personales.'),
 ('Hogar y Jardín', 'Muebles, decoración, herramientas y artículos para el cuidado del hogar y exteriores.'),
 ('Libros y Medios', 'Ficción, no ficción, revistas y contenido multimedia digital o físico.');
 
 -- marcas
-INSERT INTO marcas (nombre, descripcion) VALUES
+INSERT INTO `marcas` (nombre, descripcion) VALUES
 ('TechNova', 'Líder en innovación de hardware y software para el mercado global.'),
 ('FashionTrend', 'Marca especializada en ropa urbana, calzado y accesorios de alta calidad.'),
 ('HomeBliss', 'Ofrece soluciones prácticas y estéticas para la decoración y organización del hogar.'),
@@ -56,7 +56,7 @@ INSERT INTO `estados_transferencias_inventarios` (`nombre`) VALUES
 ('Rechazado');
 
 -- 
-INSERT INTO permisos (codigo, nombre) VALUES
+INSERT INTO `permisos` (codigo, nombre) VALUES
 ('VIEW_DASH', 'Ver Dashboard'),
 ('MANAGE_USERS', 'Gestionar Usuarios'),
 ('EDIT_PRODUCTS', 'Editar Productos'),
@@ -65,7 +65,7 @@ INSERT INTO permisos (codigo, nombre) VALUES
 ('ANULAR_DOC', 'Anular Documentos');
 
 -- 
-INSERT INTO roles (codigo, nombre) VALUES
+INSERT INTO `roles` (codigo, nombre) VALUES
 ('ADMIN', 'Administrador del Sistema'),
 ('EMPLOYEE', 'Empleado de Ventas'),
 ('MANAGER', 'Gerente de Sucursal'),
@@ -75,7 +75,7 @@ INSERT INTO roles (codigo, nombre) VALUES
 
 --- NECESITAMOS REVISAR DETALLADAMENTE ESTOS INSERTS YA QUE DEBEN COINCIDIR CON LOS IDS CREADOS PREVIAMENTE EN LA TABLA ROLES Y PERMISOS
 -- 
-INSERT INTO roles_permisos (rol_id, permiso_id) VALUES
+INSERT INTO `roles_permisos` (rol_id, permiso_id) VALUES
 -- Permisos para Administrador (rol_id = 1)
 (1, (SELECT id FROM permisos WHERE codigo = 'VIEW_DASH')),
 (1, (SELECT id FROM permisos WHERE codigo = 'MANAGE_USERS')),
