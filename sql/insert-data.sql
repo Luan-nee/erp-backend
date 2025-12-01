f5761e1cfef4cd2294c79ba05f8f614fa795eb8e
Luan-nee <luandelsol54@gmail.com>
2025-11-30 19:35:59 -0500

    agregando insert para agregar productos

--- sql/insert-data.sql
+++ sql/insert-data.sql
@@ -63,9 +63,9 @@ INSERT INTO `estados_transferencias_inventarios` (`nombre`) VALUES
 
 -- sucursales
 INSERT INTO `sucursales` (nombre, direccion, serie_factura, numero_factura_inicial, serie_boleta, numero_boleta_inicial, codigo_anexo) VALUES
-('Sucursal Central', 'Av. Principal #123, Ciudad A', 'F001', 1, 'B001', 1, 'SCENT'),
-('Sucursal Norte', 'Calle del Sol #45, Zona Norte', 'F002', 1, 'B002', 1, 'SNORT'),
-('Sucursal Sur', 'Blvd. Las Flores #67, Colonia Sur', 'F003', 1, 'B003', 1, 'SSUR');
+('Sucursal Central', 'Av. Principal #123, Ciudad A', 'F001', 1, 'B001', 1, '0001'),
+('Sucursal Norte', 'Calle del Sol #45, Zona Norte', 'F002', 1, 'B002', 1, '0002'),
+('Sucursal Sur', 'Blvd. Las Flores #67, Colonia Sur', 'F003', 1, 'B003', 1, '0003');
 
 -- 
 INSERT INTO `permisos` (codigo, nombre) VALUES
