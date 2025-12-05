4360d49372a35d2ab6585d029f490d5f6ee3cc5a
Luan-nee <luandelsol54@gmail.com>
2025-12-05 14:49:39 -0500

    factorizando el método get para endpoints similares

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,21 @@
 import express from "express";
 import type { Request, Response } from "express";
-import type { Connection } from "mysql2/promise"
+import type { Connection } from "mysql2/promise";
 import { createConnection } from "mysql2/promise";
 import dotenv from "dotenv";
-import cors from 'cors';
-import { getColores } from "./api/getColores";
-import { getMarcas } from "./api/getMarcas";
-import { getCategorias } from "./api/getCategorias";
-import { getSucursales } from "./api/getSucursales";
-import { getEstadosDocFacturacion } from "./api/getEstadosDocFacturacion";
-import { getEstadosTransferenciasInventarios } from "./api/getEstadosTransferenciasInventarios";
-import { getPermisos } from "./api/getPermisos";
-import { getMetodosPago } from "./api/getMetodosPago";
-import { getProductos } from "./api/getProductos";
-import { getRolesPermisos } from "./api/getRolesPermisos";
-import { getRoles } from "./api/getRoles";
-import { getTipoDocFacturacion } from "./api/getTipoDocFacturacion";
-import { getTiposDocumentosCliente } from "./api/getTiposDocumentosCliente";
-import { getTiposTax } from "./api/getTiposTax";
-import { getResumenCategorias } from "./api/getResumenCategorias";
+import cors from "cors";
+import GET from "./test/get";
+
+type getActionType = {
+  endpoint: string;
+  sql: string;
+  name: string;
+};
 
 dotenv.config();
 
 const corsOptions = {
-    origin: 'http://localhost:5173' // Reemplaza con el origen de tu frontend
+  origin: "http://localhost:5173", // Reemplaza con el origen de tu frontend
 };
 
 const PORT = process.env.BACKEND_PORT || 3000;
@@ -33,56 +25,111 @@ app.use(cors(corsOptions)); // 2. Usa el middleware de CORS con las opciones def
 app.use(express.json());
 let connection: Connection;
 
-(async () => { 
+(async () => {
   try {
-  connection = await createConnection({
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    // Aseguramos que password y database sean strings (mysql2 TS exige string)
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "erp_app", // Usamos la DB por defecto si no está en .env
-    port: parseInt(process.env.DB_PORT || "3306", 10),
-  });
-} catch (error) {
-  console.error("❌ Error al conectar con la base de datos:", error);
-  // Re-lanzar el error original para depuración
-  throw error as Error;
-}})();
+    connection = await createConnection({
+      host: process.env.DB_HOST || "localhost",
+      user: process.env.DB_USER || "root",
+      // Aseguramos que password y database sean strings (mysql2 TS exige string)
+      password: process.env.DB_PASSWORD || "",
+      database: process.env.DB_NAME || "erp_app", // Usamos la DB por defecto si no está en .env
+      port: parseInt(process.env.DB_PORT || "3306", 10),
+    });
+  } catch (error) {
+    console.error("❌ Error al conectar con la base de datos:", error);
+    // Re-lanzar el error original para depuración
+    throw error as Error;
+  }
+})();
 
 app.get("/", (req: Request, res: Response) => {
   // TypeScript nos ayuda con los tipos de 'req' y 'res'
   res.send("¡Hola, mundo! Esta es mi primera API con TypeScript y Express.");
 });
 
-app.get("/api/categorias", async (req: Request, res: Response) => getCategorias(req, res, connection));
+const getAction: getActionType[] = [
+  {
+    endpoint: "/api/categorias",
+    sql: "SELECT * FROM categorias;",
+    name: "categorias",
+  },
+  {
+    endpoint: "/api/colores",
+    sql: "SELECT * FROM colores;",
+    name: "colores",
+  },
+  {
+    endpoint: "/api/estados-doc-facturacion",
+    sql: "SELECT * FROM estados_doc_facturacion;",
+    name: "estados de doc facturacion",
+  },
+  {
+    endpoint: "/api/estados-transferencias-inventarios",
+    sql: "SELECT * FROM estados_transferencias_inventarios;",
+    name: "estados de transferencias inventarios",
+  },
+  {
+    endpoint: "/api/marca",
+    sql: "SELECT * FROM marcas;",
+    name: "marcas",
+  },
+  {
+    endpoint: "/api/metodos-pago",
+    sql: "SELECT * FROM metodos_pago;",
+    name: "metodos de pago",
+  },
+  {
+    endpoint: "/api/permisos",
+    sql: "SELECT * FROM permisos;",
+    name: "permisos",
+  },
+  {
+    endpoint: "/api/productos",
+    sql: "SELECT * FROM productos;",
+    name: "productos",
+  },
+  {
+    endpoint: "/api/resumen-categorias",
+    sql: "SELECT COUNT(*) AS total_categorias, CAST(SUM(cantidad_productos) AS UNSIGNED) AS total_productos, CAST(AVG(cantidad_productos) AS DECIMAL(10, 2)) AS promedio_categoria FROM `vw_categorias`;",
+    name: "resumen de categorias",
+  },
+  {
+    endpoint: "/api/roles",
+    sql: "SELECT * FROM roles;",
+    name: "roles",
+  },
+  {
+    endpoint: "/api/roles-permisos",
+    sql: "SELECT * FROM roles_permisos;",
+    name: "roles permisos",
+  },
+  {
+    endpoint: "/api/sucursales",
+    sql: "SELECT * FROM sucursales;",
+    name: "sucursales",
+  },
+  {
+    endpoint: "/api/tipos-doc-facturacion",
+    sql: "SELECT * FROM tipos_doc_facturacion;",
+    name: "tipos doc facturacion",
+  },
+  {
+    endpoint: "/api/tipos-documentos-cliente",
+    sql: "SELECT * FROM tipos_documentos_cliente;",
+    name: "tipos documentos cliente",
+  },
+  {
+    endpoint: "/api/tipos-tax",
+    sql: "SELECT * FROM tipos_tax;",
+    name: "tipos tax",
+  },
+];
 
-app.get("/api/resumen-categorias", async (req: Request, res: Response) => getResumenCategorias(req, res, connection));
-
-app.get("/api/colores", async (req: Request, res: Response) => getColores(req, res, connection));
-
-app.get("/api/estados-doc-facturacion", async (req: Request, res: Response) => getEstadosDocFacturacion(req, res, connection));
-
-app.get("/api/estados-transferencias-inventarios", async (req: Request, res: Response) => getEstadosTransferenciasInventarios(req, res, connection));
-
-app.get("/api/marcas", async (req: Request, res: Response) => getMarcas(req, res, connection));
-
-app.get("/api/metodos-pago", async (req: Request, res: Response) => getMetodosPago(req, res, connection));
-
-app.get("/api/permisos", async (req: Request, res: Response) => getPermisos(req, res, connection));
-
-app.get("/api/productos", async (req: Request, res: Response) => getProductos(req, res, connection));
-
-app.get("/api/roles-permisos", async (req: Request, res: Response) => getRolesPermisos(req, res, connection)); 
-
-app.get("/api/roles", async (req: Request, res: Response) => getRoles(req, res, connection));
-
-app.get("/api/sucursales", async (req: Request, res: Response) => getSucursales(req, res, connection));
-
-app.get("/api/tipos-doc-facturacion", async (req: Request, res: Response) => getTipoDocFacturacion(req, res, connection));
-
-app.get("/api/tipos-documentos-cliente", async (req: Request, res: Response) => getTiposDocumentosCliente(req, res, connection));
-
-app.get("/api/tipos-tax", async (req: Request, res: Response) => getTiposTax(req, res, connection));
+getAction.map(({sql, endpoint, name})=>{
+  app.get(endpoint, async (req: Request, res: Response) =>
+    GET(req, res, connection, sql, name)
+  );
+});
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
---
+++ src/test/GET.ts
@@ -0,0 +1,69 @@
+import type { Request, Response } from "express";
+import type { Connection } from "mysql2/promise";
+import type { PropProducto } from "../types/producto";
+import type { PropColor } from "../types/color";
+import type { PropMarca } from "../types/marca";
+import type {
+  PropCategoria,
+  PropResumenCategoria,
+} from "../types/categoria.js";
+import type { PropSucursal } from "../types/sucursal.js";
+import type { PropEstadosDocFacturacion } from "../types/estadosDocFacturacion.js";
+import type { PropEstadosTransferenciasInventarios } from "../types/estadosTransferenciasInventarios.js";
+import type { PropMetodopago } from "../types/metodoPago.js";
+import type { PropPermiso } from "../types/permiso.js";
+import type { PropRolesPermiso } from "../types/rolesPermiso.js";
+import type { PropRol } from "../types/rol.js";
+import type { PropTipoDocFacturacion } from "../types/tipoDocFacturacion.js";
+import type { PropTipoDocumentoCliente } from "../types/tipo_documento_cliente.js";
+import type { PropTipoTax } from "../types/tipo_tax.js";
+
+type Result =
+  | PropProducto[]
+  | PropColor[]
+  | PropMarca[]
+  | PropCategoria[]
+  | PropResumenCategoria[]
+  | PropSucursal[]
+  | PropEstadosDocFacturacion[]
+  | PropEstadosTransferenciasInventarios[]
+  | PropMetodopago[]
+  | PropPermiso[]
+  | PropRolesPermiso[]
+  | PropRol[]
+  | PropTipoDocFacturacion[]
+  | PropTipoDocumentoCliente[]
+  | PropTipoTax[];
+
+export type PropResponse = {
+  status: number;
+  message: string;
+  info: Result | null;
+};
+
+export default async function GET(
+  req: Request,
+  res: Response,
+  connection: Connection,
+  sql: string,
+  nameEntidad: string
+): Promise<void> {
+  try {
+    const [results] = await connection.execute(sql);
+    const response: PropResponse = {
+      status: 200,
+      message: `${nameEntidad} obtenidos con éxito.`,
+      info: results as Result,
+    };
+    res.status(200).json(response);
+    console.log(`✅ (GET: ${nameEntidad}) - ejecutada con éxito.`);
+  } catch (error) {
+    const response: PropResponse = {
+      status: 500,
+      message: `Error interno del servidor al obtener ${nameEntidad}.`,
+      info: null,
+    };
+    res.status(500).json(response);
+    console.log(`❌ (Error: ${nameEntidad}) - error en la consulta`, error);
+  }
+}
