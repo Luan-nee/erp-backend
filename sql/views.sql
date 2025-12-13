71e77c3e9d19e7457672196a6b75ca2861b0b5cc
Luan-nee <luandelsol54@gmail.com>
2025-12-13 09:15:44 -0500

    Creando vista para marcas

--- sql/views.sql
+++ sql/views.sql
@@ -6,3 +6,16 @@ SELECT categorias.id,
 FROM categorias
 JOIN productos ON categorias.id = productos.categoria_id
 GROUP BY categorias.id, categorias.nombre, categorias.descripcion;
+
+
+
+CREATE VIEW `erp_app`.`vw_marcas` AS
+  SELECT 
+      `erp_app`.`marcas`.`id` AS `id`,
+      `erp_app`.`marcas`.`nombre` AS `nombre`,
+      `erp_app`.`marcas`.`descripcion` AS `descripcion`,
+      COUNT(`erp_app`.`marcas`.`id`) AS `cantidad_productos`
+  FROM
+      (`erp_app`.`marcas`
+      JOIN `erp_app`.`productos` ON ((`erp_app`.`marcas`.`id` = `erp_app`.`productos`.`marca_id`)))
+  GROUP BY `erp_app`.`marcas`.`id` , `erp_app`.`marcas`.`nombre` , `erp_app`.`marcas`.`descripcion`
