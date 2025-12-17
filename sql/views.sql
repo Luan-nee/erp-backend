c804b0b3a3beb8e30e76a60eb08964cd181ee7d7
Luan-nee <luandelsol54@gmail.com>
2025-12-17 10:16:20 -0500

    arreglando la sintanxis del view de marcas

--- sql/views.sql
+++ sql/views.sql
@@ -1,8 +1,8 @@
 CREATE VIEW `vw_categorias` AS
 SELECT categorias.id,
-       categorias.nombre,
-       categorias.descripcion,
-       COUNT(productos.categoria_id) AS 'cantidad_productos'
+    categorias.nombre,
+    categorias.descripcion,
+    COUNT(productos.categoria_id) AS 'cantidad_productos'
 FROM categorias
 LEFT JOIN productos ON categorias.id = productos.categoria_id
 GROUP BY categorias.id, categorias.nombre, categorias.descripcion;
@@ -10,12 +10,12 @@ GROUP BY categorias.id, categorias.nombre, categorias.descripcion;
 
 
 CREATE VIEW `erp_app`.`vw_marcas` AS
-  SELECT 
-      `erp_app`.`marcas`.`id` AS `id`,
-      `erp_app`.`marcas`.`nombre` AS `nombre`,
-      `erp_app`.`marcas`.`descripcion` AS `descripcion`,
-      COUNT(`erp_app`.`marcas`.`id`) AS `cantidad_productos`
-  FROM
-      (`erp_app`.`marcas`
-      JOIN `erp_app`.`productos` ON ((`erp_app`.`marcas`.`id` = `erp_app`.`productos`.`marca_id`)))
-  GROUP BY `erp_app`.`marcas`.`id` , `erp_app`.`marcas`.`nombre` , `erp_app`.`marcas`.`descripcion`
+SELECT 
+    marcas.id AS `id`,
+    marcas.nombre AS `nombre`,
+    marcas.descripcion AS `descripcion`,
+    COUNT(marcas.id) AS `cantidad_productos`
+FROM
+    (`erp_app`.`marcas`
+JOIN `erp_app`.`productos` ON ((`erp_app`.`marcas`.`id` = `erp_app`.`productos`.`marca_id`)))
+GROUP BY `erp_app`.`marcas`.`id` , `erp_app`.`marcas`.`nombre` , `erp_app`.`marcas`.`descripcion`
