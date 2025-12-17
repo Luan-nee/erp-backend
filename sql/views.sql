fc76227cd07c08a5c7d74abb72c1ab68b9f16c07
Luan-nee <luandelsol54@gmail.com>
2025-12-17 05:04:22 -0500

    Arreglando el script para crear la vista vw_categorias y ajustando el repositorio de categorías para incluir nuevas funcionalidades.

--- sql/views.sql
+++ sql/views.sql
@@ -2,9 +2,9 @@ CREATE VIEW `vw_categorias` AS
 SELECT categorias.id,
        categorias.nombre,
        categorias.descripcion,
-       COUNT(categorias.id) AS 'cantidad_productos'
+       COUNT(productos.categoria_id) AS 'cantidad_productos'
 FROM categorias
-JOIN productos ON categorias.id = productos.categoria_id
+LEFT JOIN productos ON categorias.id = productos.categoria_id
 GROUP BY categorias.id, categorias.nombre, categorias.descripcion;
 
 
