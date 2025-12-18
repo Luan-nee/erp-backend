9cddd66831990f51cb51869c34ca6b6f49194c73
Luan-nee <luandelsol54@gmail.com>
2025-12-18 18:23:41 -0500

    agregando procedimiento almacenado para eliminar marcas

--- sql/StoreProcedures.sql
+++ sql/StoreProcedures.sql
@@ -11,3 +11,17 @@ DELIMITER //
 			END IF;
 	END //
 DELIMITER ;
+
+DELIMITER //
+	CREATE PROCEDURE sp_eliminar_marca(IN p_id_marca INT)
+	BEGIN
+			IF p_id_marca <> 1 THEN
+					UPDATE productos 
+					SET marca_id = 1 
+					WHERE marca_id = p_id_marca;
+
+					DELETE FROM marcas 
+					WHERE id = p_id_marca;					
+			END IF;
+	END //
+DELIMITER ;
