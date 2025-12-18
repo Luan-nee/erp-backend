98ea2b1dfaf2fee024dff762d07ffd86884527b7
Luan-nee <luandelsol54@gmail.com>
2025-12-18 15:36:17 -0500

    Agregando una categoria por defecto para productos sin categoria asignada.

--- sql/data/categorias.sql
+++ sql/data/categorias.sql
@@ -1,4 +1,5 @@
 INSERT INTO `categorias` (nombre, descripcion) VALUES
+('sin categoria', 'Todos los productos que no tienen una categoria asignada aun, será incluida en esta categoria por defecto.'),
 ('Electrónica', 'Dispositivos y accesorios tecnológicos como laptops, móviles y audífonos.'),
 ('Ropa y Moda', 'Artículos de vestimenta, calzado y accesorios personales.'),
 ('Hogar y Jardín', 'Muebles, decoración, herramientas y artículos para el cuidado del hogar y exteriores.'),
