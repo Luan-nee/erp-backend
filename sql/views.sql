36c028f6083651f87cb457e35211e5c5db2c6a3f
Luan-nee <luandelsol54@gmail.com>
2025-12-19 15:56:47 -0500

    Arreglando vistas para contar correctamente la cantidad de productos por categoria y marca.

--- sql/views.sql
+++ sql/views.sql
@@ -1,7 +1,8 @@
 CREATE VIEW `vw_categorias` AS
-SELECT categorias.id,
-    categorias.nombre,
-    categorias.descripcion,
+SELECT 
+    categorias.id AS `id`,
+    categorias.nombre AS `nombre`,
+    categorias.descripcion AS `descripcion`,
     COUNT(productos.categoria_id) AS 'cantidad_productos'
 FROM categorias
 LEFT JOIN productos ON categorias.id = productos.categoria_id
@@ -9,13 +10,12 @@ GROUP BY categorias.id, categorias.nombre, categorias.descripcion;
 
 
 
-CREATE VIEW `erp_app`.`vw_marcas` AS
+CREATE VIEW `vw_marcas` AS
 SELECT 
     marcas.id AS `id`,
     marcas.nombre AS `nombre`,
     marcas.descripcion AS `descripcion`,
-    COUNT(marcas.id) AS `cantidad_productos`
-FROM
-    (`erp_app`.`marcas`
-LEFT JOIN `erp_app`.`productos` ON ((`erp_app`.`marcas`.`id` = `erp_app`.`productos`.`marca_id`)))
-GROUP BY `erp_app`.`marcas`.`id` , `erp_app`.`marcas`.`nombre` , `erp_app`.`marcas`.`descripcion`
+    COUNT(productos.marca_id) AS `cantidad_productos`
+FROM marcas
+LEFT JOIN productos ON marcas.id = productos.marca_id
+GROUP BY marcas.id , marcas.nombre , marcas.descripcion;
