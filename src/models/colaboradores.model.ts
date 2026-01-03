c7842bf1e5f2a860b84294dd8fa59b57e4d2bc04
Luan-nee <luandelsol54@gmail.com>
2026-01-03 11:55:35 -0500

    Agregando tipo Colaborador y repositories para la gestión de colaboradores

---
+++ src/models/colaboradores.model.ts
@@ -0,0 +1,9 @@
+
+export type Colaborador = {
+  nombres: string;
+  apellidos: string;
+  rol: string; // admin, vendedor, cajero.
+  estaActivo: boolean;
+  celular: string;
+  lugarTrabajo: string;
+}
---
+++ src/repositories/colaboradores.repository.ts
@@ -0,0 +1,29 @@
+import { db } from "../config/db.config";
+import type { Colaborador } from "../models/colaboradores.model";
+import { RowDataPacket, ResultSetHeader } from "mysql2";
+
+export class ColaboradoresRepository {
+  async select(): Promise<Colaborador[] | null> {
+    const [rows] = await db.query<RowDataPacket[]>(
+      `
+      select 
+        u.nombres as 'nombres', 
+        u.apellidos as 'apellidos', 
+        rol.nombre as 'rol',
+        u.estaActivo as 'estaActivo',
+        u.celular as 'celular',
+        suc.direccion as 'lugarTrabajo'
+      from 
+        usuarios as u join 
+        cuentas_usuario as cu join 
+        roles as rol join 
+        sucursales as suc
+      ON (u.sucursal_id = suc.id AND cu.usuario_id = u.id AND  cu.rol_id = rol.id);
+      `
+    );
+    if (rows.length === 0) {
+      return null;
+    }
+    return rows as Colaborador[];
+  }
+}
