fb290e14f2c1131acdadbb736f250174a5d4c990
Luan-nee <luandelsol54@gmail.com>
2026-01-03 12:34:28 -0500

    Arreglando nomenclatura del rol de cajero.

--- sql/data/roles.sql
+++ sql/data/roles.sql
@@ -1,4 +1,4 @@
 INSERT INTO `roles` (codigo, nombre) VALUES
 ('ADMINISTRADOR', 'Administrador del Sistema'),
 ('VENDEDOR', 'Empleado de Ventas'),
-('CAJERO', 'Gerente de Sucursal');
+('CAJERO', 'Cajero de Caja');
